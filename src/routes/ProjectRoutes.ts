1eaa5d6a2d23d3dedc32d85edb4726cbeb244d8b
Christian <chrisdaq@gmail.com>
2025-08-02 15:53:44 -0400

    chore: add express-validator, verify data at route level, add custom middleware to show validation errors

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
 router.get("/", ProjectController.getAllProjects);
-router.post("/", ProjectController.addProject);
+router.post(
+  "/",
+  body("projectName").notEmpty().withMessage("Project name is required"),
+  body("clientName").notEmpty().withMessage("Client name is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  handleInputErrors,
+  ProjectController.addProject,
+);
 
 export default router;
