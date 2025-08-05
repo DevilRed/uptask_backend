28c082fc266940398a0c920fb601e81c5a42b7f7
Christian <chrisdaq@gmail.com>
2025-08-05 16:10:16 -0400

    add status attribute to Task model

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,21 @@
 import { Document, model, Schema, Types } from "mongoose";
 
+// define an object to act as a dictionary for status attribute
+const taskStatus = {
+  PENDING: 'pending',
+  ON_HOLD: 'onHold',
+  IN_PROGRESS: 'inProgress',
+  UNDER_REVIEW: 'underReview',
+  COMPLETED: 'completed'
+} as const
+
+// keyof: to set tasks status value to be only 'pending', 'onHold' ....
+export type TasksStatus = typeof taskStatus[keyof typeof taskStatus]
 export interface ITask extends Document {
   name: string;
   description: string;
   project: Types.ObjectId;
+  status: TasksStatus
 }
 
 export const TaskSchema = new Schema<ITask>(
@@ -11,6 +23,7 @@ export const TaskSchema = new Schema<ITask>(
     name: { type: String, required: true },
     description: { type: String, required: true },
     project: { type: Schema.Types.ObjectId, ref: "Project" },
+    status: { type: String, enum: Object.values(taskStatus), default: taskStatus.PENDING }
   },
   { timestamps: true },
 );
