9315375b3c1c7d584026300b977d8a836385ac6b
Christian <chrisdaq@gmail.com>
2025-08-22 15:48:50 -0400

    enable cors on app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import ProjectRoutes from "./routes/ProjectRoutes";
 
+dotenv.config()
 connectDB();
 const app = express();
+app.use(cors(corsConfig))
 app.use(express.json());
 app.use("/api/projects", ProjectRoutes);
 
