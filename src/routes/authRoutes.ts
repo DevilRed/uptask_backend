45b83e94b9e6ecfe253f2ab720993f68b96f0799
Christian <chrisdaq@gmail.com>
2025-09-11 15:22:10 -0400

    add auth controller and its route along fields validation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,23 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-	console.log('from auth route');
-	res.json({ message: 'Auth route working correctly' });
-});
+router.post("/create-account",
+	body("name")
+		.notEmpty().withMessage("Name is required"),
+	body("email")
+		.isEmail().withMessage("Invalid email"),
+	body("password")
+		.notEmpty().withMessage("Password is required")
+		.isLength({ min: 8 }).withMessage("Password must be at least 8 characters long"),
+	body("password_confirmation")
+		.custom((value, { req }) => value === req.body.password)
+		.withMessage("Passwords do not match"),
+	handleInputErrors,
+	AuthController.createAccount
+);
 
 export default router;
