ac40638e12d8904fbb6529c0a8995e18c46d1b17
Christian <chrisdaq@gmail.com>
2025-09-15 16:56:58 -0400

    add bcrypt package to hash passwords, use it at createAccount event with auth util

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express"
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
 	static createAccount = async (req: Request, res: Response) => {
 		try {
 			const { name, email, password } = req.body;
-			await User.create({ name, email, password });
+			const user = new User({ name, email, password });
+			user.password = await hashPassword(password);
+			await user.save();
 			res.status(201).send('Account created successfully, check your email to confirm your account');
 		} catch (error) {
 			res.status(500).json({ error: error.message })
