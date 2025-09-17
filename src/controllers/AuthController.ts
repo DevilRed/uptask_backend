6a3b04fe2dcbc8f76a466af824b2f59834cfa5df
Christian <chrisdaq@gmail.com>
2025-09-17 09:04:00 -0400

    send email on account save

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { transporter } from "../config/nodemailer";
+import Token from "../models/Token";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
-import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 
 export class AuthController {
@@ -21,6 +22,15 @@ export class AuthController {
 			const token = new Token()
 			token.token = generateToken()
 			token.user = user.id
+
+			// email send
+			await transporter.sendMail({
+				from: 'UpTask <admin@uptask.com>',
+				to: user.email,
+				subject: 'UpTask - account confirmation',
+				text: 'UpTask - account confirmation',
+				html: '<p>email testing</p>'
+			})
 
 			// await user.save();
 			// await token.save()
