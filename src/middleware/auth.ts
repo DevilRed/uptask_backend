669112fb0d16c02fc5818b3796c4f5c1f99d9b0d
Christian <chrisdaq@gmail.com>
2025-09-25 18:46:04 -0400

    change token duration from 6 minutes to 180 days, verify user id of token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
 	// get token from headers
@@ -9,6 +11,14 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
 	}
 	// destructure token
 	const token = bearer.split(' ')[1]
-	console.log(token);
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET!) as jwt.JwtPayload
+		const user = await User.findById(decoded.id)
+		console.log(user);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: 'Invalid token' })
+	}
 	next()
 }
